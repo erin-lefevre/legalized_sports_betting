a0f8ec20ccc6f15c2a18ede10aa622fabf1c9d6d
Erin LeFevre <46400555+erin-lefevre@users.noreply.github.com>
2020-12-12 13:22:23 -0700

    Update comments in main.js
    Updated all comments in main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /*JavaScript - Erin J. LeFevre, Jeff Kalar 2020*/
 
-
+/*Create map*/
 var myMap = L.map('mapid', {
    center: [42, -100],
    zoom: 4,
@@ -14,20 +14,20 @@ var myMap = L.map('mapid', {
     
 });
 
-/*Background map tile layer 1*/
+/*Default map tile layer 1*/
 var basemap1 = L.tileLayer('https://api.mapbox.com/styles/v1/erin-lefevre/ckg03iir70ezk19qrtlwbzwtu/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXJpbi1sZWZldnJlIiwiYSI6ImNrZnljaWQ2eDBvd3gzM283eDdxaW03ZmwifQ.pUnzIpTy98k-H2VfbkPFkA', {
     
     maxZoom: 18,
     
 }).addTo(myMap);
 
-/*Background map tile layer 2*/
+/*Secondary tile layer - aerial imagery*/
 var basemap2 = L.tileLayer('https://api.mapbox.com/styles/v1/erin-lefevre/ckg05kat80jec19lan4o72erl/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXJpbi1sZWZldnJlIiwiYSI6ImNrZnljaWQ2eDBvd3gzM283eDdxaW03ZmwifQ.pUnzIpTy98k-H2VfbkPFkA', {           
             maxZoom: 18
         });
 
 
-/*New marker for legal states*/
+/*Marker for legal states*/
 var goldcoin_legal = new L.Icon({
     iconUrl: 'images/goldcoin_legal.png',
     iconRetinaUrl: 'images/goldcoin_legal.png',    
@@ -37,6 +37,7 @@ var goldcoin_legal = new L.Icon({
     
   });
 
+/*Add legal states data*/
 var legalStatesGeoJSON = L.geoJSON(states_legal_points, {
       pointToLayer: function(feature, latlng) {
         console.log(latlng, feature);
@@ -48,9 +49,7 @@ var legalStatesGeoJSON = L.geoJSON(states_legal_points, {
     }).addTo(myMap);
 
 
-
-
-/*New marker for states where it isn't legal to gamble*/
+/*Marker for illegal states*/
 var goldcoin_legal_no = new L.Icon({
     iconUrl: 'images/goldcoin_legal_no.png',
     iconRetinaUrl: 'images/goldcoin_legal_no.png',    
@@ -60,15 +59,7 @@ var goldcoin_legal_no = new L.Icon({
     
   });
 
-function onEachFeature(feature, layer) {
-    console.log(feature);
-     layer.bindPopup('<b>'+'State Name: '+'</b>'+ feature.properties.Name +
-	 '<br>'+ '<b>'+'Status: '+'</b>'+ feature.properties.Status	+
-	 '<br>'+ '<b>'+'Casinos: '+'</b>'+ feature.properties.Casinos
-	  +'<br>'+ '<b>'+'Rules: '+'</b>'+ feature.properties.Rules);
-  }
-
-
+/*Add illegal states data*/
 var nolegalStatesGeoJSON = L.geoJSON(states_legal_no_points, {
       pointToLayer: function(feature, latlng) {
         console.log(latlng, feature);
@@ -80,7 +71,7 @@ var nolegalStatesGeoJSON = L.geoJSON(states_legal_no_points, {
     }).addTo(myMap);
 
 
-/*New marker for states with an active bill*/
+/*Marker for states with an active bill*/
 var goldcoin_active = new L.Icon({
     iconUrl: 'images/goldcoin_active.png',
     iconRetinaUrl: 'images/goldcoin_active.png',    
@@ -90,7 +81,7 @@ var goldcoin_active = new L.Icon({
     
   });
 
-
+/*Add active states data*/
 var states_active_points = L.geoJSON(states_active_points, {
       pointToLayer: function(feature, latlng) {
         console.log(latlng, feature);
@@ -102,20 +93,29 @@ var states_active_points = L.geoJSON(states_active_points, {
     }).addTo(myMap);
 
 
+/*Popup information for all layers*/
+function onEachFeature(feature, layer) {
+    console.log(feature);
+     layer.bindPopup('<b>'+'State Name: '+'</b>'+ feature.properties.Name +
+	 '<br>'+ '<b>'+'Status: '+'</b>'+ feature.properties.Status	+
+	 '<br>'+ '<b>'+'Casinos: '+'</b>'+ feature.properties.Casinos
+	  +'<br>'+ '<b>'+'Rules: '+'</b>'+ feature.properties.Rules);
+  }
 
-
-/*Layer control start*/
+/*Layer control basemaps*/
 var basemaps = {
     "Gold & Black": basemap1,
     "Aerial Imagery": basemap2
   };
 
+/*Layer control overlays*/
 var overlays = {   
 	"Legal States": legalStatesGeoJSON,
 	"Active bill": states_active_points,
     "Illegal States": nolegalStatesGeoJSON
   };
 
+/*add all layers to the layer control - checkbox on by default*/
 L.control.layers(basemaps, overlays, {
     collapsed: false
 }).addTo(myMap);
